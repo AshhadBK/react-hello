e5a8a8dfec57251e9167aacd39c6b107b0b1a356
Ashhad <ashhadbinkashif@gmail.com>
2020-10-05 01:56:05 +0500

    Update index.js
    A component must return a single element. A component can have multiple components in itself

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function HelloWorld(){
-    // return (
-    // // <div>Hello World</div> //--> Returning JSX
-    // );
+    return (
+    <div><Hello/> <World/>!</div> //--> A component must return a single element. A component can have multiple components in itself.
+    );
 
-    return React.createElement('div', {}, "Hello", " ", "World","!");
+    // return React.createElement('div', {}, "Hello", " ", "World","!");
 }
+
+function Hello(){
+    return(
+        <span>Hello</span>
+    );
+}
+
+function World(){
+    return(
+        <span> World</span>
+    );
+}
+
 
 ReactDOM.render(
     <HelloWorld/>,
