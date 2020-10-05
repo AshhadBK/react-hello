c0573acf9f89e384750626e05cfb76d2ceca32a5
Ashhad <ashhadbinkashif@gmail.com>
2020-10-05 19:17:06 +0500

    Update index.js
    Displayed multiple components using Fragment tags <> </>.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// function HelloWorld(){
+//     // return [<Hello/>, <World/>, "!"];
+//      //--> A component can return an array too. After all it is JS in JSX.
+
+
+//     // return React.createElement('div', {}, "Hello", " ", "World","!");
+
+//     // To return multiple elements, eithre wrap them within a single tag or use fragments.
+//     // function HelloWorld() {
+//     // return (
+//     //     <div>
+//     //     <Hello/> <World/>!
+//     //     </div>
+//     //     );
+//     //     }
+
+//     // Fragments:
+    
+
+// }
+
+// 
+
+//Fragments Example:
 function HelloWorld(){
-    return [<Hello/>, <World/>, "!"];
-     //--> A component can return an array too. After all it is JS in JSX.
+return(
+<>
+    <Hello/>
+    <World/>
+    !
+</>
 
+)
 
-    // return React.createElement('div', {}, "Hello", " ", "World","!");
 }
+
 
 function Hello(){
     return(
